a5819217abd2f4d78bdc78d99ff552adaa5498a6
ahmet karayol <37154452+akarayol03@users.noreply.github.com>
2020-05-18 11:28:25 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,6 @@
-# Start a container
-resource "docker_container" "ubuntu" {
-  name  = "foo"
-  image = "${docker_image.ubuntu.latest}"
-}
-
 # Find the latest Ubuntu precise image.
 resource "docker_image" "ubuntu" {
   name = "ubuntu:precise"
 }
+
+# Access it somewhere else with ${docker_image.ubuntu.latest}
